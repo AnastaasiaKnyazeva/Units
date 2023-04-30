6254f2e5c70780f1640efef7d575d584198ca9c2
admin <e@w.com>
2023-04-30 19:34:29 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Units;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StringAssert.Contains("stroka", Program.test5());
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(10, Program.test1(5));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreNotEqual(10, Program.test2(5));
+        }
+
+
+
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.IsTrue(Program.test3(9));
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.IsFalse(Program.test3(-3));
+        }
+
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Assert.IsInstanceOfType(Program.test1(3),typeof(int));
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Assert.IsNotInstanceOfType(Program.test1(3), typeof(string));
+        }
+
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            Assert.IsNull(Program.test4(-3));
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Assert.IsNotNull(Program.test4(3));
+        }
+
+
+        
+    }
+}
---
+++ Units/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Units
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int test1(int x)
+        {
+            return x + x;
+        }
+        public static int test2(int x)
+        {
+            return x * x;
+        }
+        public static Boolean test3(int x)
+        {
+            if (x > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static object test4(int x)
+        {
+            object Pepka;
+            if (x > 0)
+            {
+                Pepka = x;
+                return Pepka;
+            }
+            else
+            {
+                Pepka = null;
+                return Pepka;
+            }
+        }
+
+        public static string test5()
+        {
+            string str = "stroka";
+            return str;
+        }
+        public static void test6()
+        {
+            List<string> collection = new List<string> { "apple", "banana", "cherry" };
+            Console.WriteLine(collection);
+        }
+
+
+
+
+
+
+    }
+}
